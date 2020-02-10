9bfc55a8bf92dfe72bd5748c7d454e86d6f2273c
FunmiBADev <funmiba@hotmail.com>
2020-02-10 15:24:45 +0000

    Student Names

---
+++ directory.rb
@@ -0,0 +1,15 @@
+
+
+puts "The students of Makers Apprecticeship Academy"
+puts "-------------"
+puts "Bruce Wayne"
+puts "Bruce Banner"
+puts "Barry Allen"
+puts "Iris West"
+puts "Clark Kent"
+puts "Dr. Who"
+puts "Terminator"
+puts "Nancy Drew"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
