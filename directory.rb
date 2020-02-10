3d1013492dd5ed90c25cb22fa0d4cc43f113e135
FunmiBADev <funmiba@hotmail.com>
2020-02-10 16:13:04 +0000

    Array refactoring update

--- directory.rb
+++ directory.rb
@@ -1,22 +1,33 @@
 
 ## STUDENT NAMES
 
-student_count = 11
+
+
+# Students in an Array
+
+students = [
+	"Bruce Wayne", 
+	"Bruce Banner",
+	"Barry Allen",
+	"Iris West",
+	"Clark Kent",
+	"Dr. Who",
+	"Terminator",
+	"Nancy Drew",
+	"The Joker",
+	"Joffrey Baratheon",
+	"Norman Bates"
+]
 
 puts "The students of Makers Apprecticeship Academy"
 puts "-------------"
-puts "Bruce Wayne"
-puts "Bruce Banner"
-puts "Barry Allen"
-puts "Iris West"
-puts "Clark Kent"
-puts "Dr. Who"
-puts "Terminator"
-puts "Nancy Drew"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+
+students.each do |student|
+	puts student
+end
+
+
 
 ## USE OF PRINT INSTEAD OF PUTS
 
-puts "Overall, we have " #{student_count} " great students"
+puts "Overall, we have #{students.count} great students"
