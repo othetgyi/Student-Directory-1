4d9e6d0efc94d79a5826c2543f718be891b48ced
Olivia Thetgyi <othetgyi@gmail.com>
2020-02-11 16:36:26 +0000

    length

--- directory.rb
+++ directory.rb
@@ -48,6 +48,7 @@ end
 def print(students)
 	students.each do |student|
 	#	if student[:name].start_with? ("A")
+	if student[:name].length < 12
 			puts "#{student[:name]} (#{student[:cohort]} cohort). "
 		end
 	end
