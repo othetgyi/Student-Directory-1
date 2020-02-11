1d731eeb6dfaa5dad28cc2a84c725c45e60c3c5b
Olivia Thetgyi <othetgyi@gmail.com>
2020-02-11 14:51:08 +0000

    added methods

--- directory.rb
+++ directory.rb
@@ -5,6 +5,7 @@
 
 # Students in an Array
 
+=begin
 students = [
 {name: "Bruce Wayne", cohort: :February},
 {name: "Bruce Banner", cohort: :February},
@@ -18,10 +19,26 @@ students = [
 {name: "Joffrey Baratheon", cohort: :February},
 {name: "Norman Bates", cohort: :February},
 
-
 ]
-
-
+=end
+ def input_students
+	 puts "Please enter the names of the students"
+	 puts "To finish, just hit return twice"
+	 # create an empty Array
+	 students = []
+	 # get the first name
+	 name = gets.chomp
+	 # while the name is not empty, repeat this code
+	 while !name.empty? do
+	 	# add the student hash to the Array
+		students << {name: name, cohort: :november}
+		puts "Nowe we have #{students.count} students"
+		# get another name from the user
+		name = gets.chomp
+	end
+# return the array of Students
+	students
+end
 
 def print_header
 	puts "The students of Makers Apprecticeship Academy"
@@ -29,7 +46,7 @@ def print_header
 end
 def print (students)
 	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort). " 
+		puts "#{student[:name]} (#{student[:cohort]} cohort). "
 	end
 
 end
@@ -39,11 +56,7 @@ def print_footer (students)
 end
 
 # Now call the methods
+students = input_students
 print_header
-print (students)
-print_footer (students)
-
-
-
-
-
+print(students)
+print_footer(students)
